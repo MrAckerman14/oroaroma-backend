6993cdc0d0a01a6a194d7fe9ce7f50197c2ba508
Guelmy <pguelmi@gmail.com>
2026-01-29 16:09:59 -0400

    config dist to render

--- src/config/router.js
+++ src/config/router.js
@@ -1,6 +1,6 @@
 import authRouter from '@/routes/auth';
 import indexRouter from '@/routes/index';
-import tweetRouter from '@/routes/tweet';
+// import tweetRouter from '@/routes/tweet';
 import users from '@/routes/user';
 import store from '@/routes/store';
 import sale from '@/routes/sale';
@@ -9,7 +9,7 @@ import report from '@/routes/report'
 export default function (app) {
   app.use('/', indexRouter);
   app.use('/auth', authRouter);
-  app.use('/tweets', tweetRouter);
+  // app.use('/tweets', tweetRouter);
   app.use('/users', users);
   app.use('/store', store);
   app.use('/sales', sale);
