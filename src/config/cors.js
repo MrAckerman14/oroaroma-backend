b96498fec0e2097d2cd8327991345035c03520b6
Aaron Moreno <morenoaaron486@gmail.com>
2026-04-18 01:28:32 -0400

    cors changed

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,7 @@
 export default {
   origin:[
       process.env.CORS_ALLOWED_ORIGIN,
+      'https://fronted-oro-aroma-u6s0.onrender.com',
       'http://localhost:9000',
       'http://192.168.1.6:9000',
       'http://192.168.1.3:9000'
