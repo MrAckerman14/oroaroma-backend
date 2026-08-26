3d907463459f02071e7aa1df9819a469a4116ce9
JorgeLuMC <jorgecoca2323@gmail.com>
2026-08-26 00:26:34 -0400

    xd

--- zoko-hola-admin-frontend/src/config/appTheme.js
+++ zoko-hola-admin-frontend/src/config/appTheme.js
@@ -3,7 +3,7 @@ import brandLogo from 'src/assets/logo.png';
 // Configuracion global de identidad.
 // Cambia este valor para actualizar el nombre visible de toda la aplicacion.
 export const appTheme = {
-  name: 'Oro Aroma',
+  name: 'Ooro Aroma',
   // Archivo ubicado en src/assets. Cambialo al crear otro tema.
   logo: brandLogo,
   // Archivo ubicado en public. Cambialo al crear otro tema.
